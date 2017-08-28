62c705ad4abfa1e18b7dcfefccc0dbda5d622f82
teralove <terascripts@gmail.com>
2017-08-28 10:32:54 -0400

    tabs to spaces

--- index.js
+++ index.js
@@ -4,42 +4,42 @@ module.exports = function AutoCleric(dispatch) {
     
     let playerCid;
     let clericNpc = undefined;
-	let enabled = true; // auto-buy heals?    
+    let enabled = true; // auto-buy heals?    
     
     const HealthTrigger = 0.95; // auto-buy heal when HP falls below this percent
     const AutoBuyDelay = 200; // ms ping        
     
-    const chatHook = event => {		
-		let command = format.stripTags(event.message).split(' ');
-		
-		if (['!cleric'].includes(command[0].toLowerCase())) {
-			toggleModule();
-			return false;
-		} 
+    const chatHook = event => {
+        let command = format.stripTags(event.message).split(' ');
+        
+        if (['!cleric'].includes(command[0].toLowerCase())) {
+            toggleModule();
+            return false;
+        } 
         else if (['!cleric.hp'].includes(command[0].toLowerCase()))
         {
             buyHeal();
             return false;
         }
     }
-	dispatch.hook('C_CHAT', 1, chatHook)	
-	dispatch.hook('C_WHISPER', 1, chatHook)  	
+    dispatch.hook('C_CHAT', 1, chatHook)
+    dispatch.hook('C_WHISPER', 1, chatHook)
     
-	// slash support
-	try {
-		const Slash = require('slash')
-		const slash = new Slash(dispatch)
-		slash.on('cleric', args => toggleModule())
-		slash.on('cleric.hp', args => buyHeal())
-	} catch (e) {
-		// do nothing because slash is optional
-	}
+    // slash support
+    try {
+        const Slash = require('slash')
+        const slash = new Slash(dispatch)
+        slash.on('cleric', args => toggleModule())
+        slash.on('cleric.hp', args => buyHeal())
+    } catch (e) {
+        // do nothing because slash is optional
+    }
     
-	function toggleModule() {
-		enabled = !enabled;
-		systemMessage((enabled ? 'enabled' : 'disabled'));
-	}
-    	
+    function toggleModule() {
+        enabled = !enabled;
+        systemMessage((enabled ? 'enabled' : 'disabled'));
+    }
+    
     function buyHeal() {        
         if (clericNpc) {
             setTimeout(() => {            
@@ -49,7 +49,7 @@ module.exports = function AutoCleric(dispatch) {
                 });
             }, AutoBuyDelay);
         }
-	}  
+    }  
     
     dispatch.hook('S_CREATURE_CHANGE_HP', 2, (event) => {
         if (!enabled) return;
@@ -63,26 +63,26 @@ module.exports = function AutoCleric(dispatch) {
         }
     })               
     
-	dispatch.hook('S_DESPAWN_NPC', 1, (event) => {	
+    dispatch.hook('S_DESPAWN_NPC', 1, (event) => {
         if ((clericNpc) && clericNpc.cid - event.target == 0)
         {
             clericNpc = undefined;
         }    
     })                
       
-	dispatch.hook('C_BUY_VILLAGER_BUFF', 1, (event) => {	
-        clericNpc = event;		
-	})
+    dispatch.hook('C_BUY_VILLAGER_BUFF', 1, (event) => {
+        clericNpc = event;
+    })
   
-	dispatch.hook('S_LOGIN', 2, (event) => {
+    dispatch.hook('S_LOGIN', 2, (event) => {
         playerCid = event.cid;
-	})
-		
-	function systemMessage(msg) {
-		dispatch.toClient('S_CHAT', 1, {
-			channel: 24, //system
-			authorName: '',
-			message: ' (cleric) ' + msg
-		});
-	}
+    })
+        
+    function systemMessage(msg) {
+        dispatch.toClient('S_CHAT', 1, {
+            channel: 24, //system
+            authorName: '',
+            message: ' (cleric) ' + msg
+        });
+    }
 }
